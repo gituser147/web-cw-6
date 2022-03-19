2446e94bee6a6b15968c334e7b1b366396acbe7a
adam alsaid kamal <adamzarzora9@gmail.com>
2022-03-19 13:15:32 +0300

    .

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,15 @@
-// هنا سوف تنشئ دالة calculate 
+// هنا سوف تنشئ دالة calculate
+function calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let BMI = weight / (height * height);
+  let status = " ";
+  if (BMI < 18.5) {
+    status = "تعاني من نقص في الوزن";
+  } else if (BMI < 25) {
+    status = "وزنك صحي";
+  }
+  BMI = BMI.toFixed(2);
+  document.getElementById("result").innerHTML = BMI;
+  document.getElementsByClassName("Comment")[0].innerHTML = status;
+}
